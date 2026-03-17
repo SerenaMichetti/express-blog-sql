ae72738f542bb686d4ff745d166466f79f387f6b
Serena <sere.michetti@icloud.com>
2026-03-17 17:55:08 +0100

    creo la chiamata di show e testata su postman

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,15 +26,33 @@ function index(req, res) {
 
 //creo una funzione di showche restituisce un post in base all'id, se l'id non è un numero restituisce un errore 400, se l'id non esiste restituisce un errore 404
 function show(req, res) {
+    
+    //se l'id non è un numero restituisco un errore 
     const id = Number(req.params.id);
     if (isNaN(id)) {
         return res.status(400).json({ error: 'User error', message: 'L\'ID deve essere un numero' })
     }
-    const result = posts.find(post => post.id == id);
-    if (!result) {
-        return res.status(404).json({ error: 'Not found', message: `Post con id ${id} non trovato` })
-    }
-    return res.json(result)
+
+    const sqlQuery = 'SELECT * FROM posts WHERE id = ?';
+    const parametriQuery = [id];
+
+    dbConnection.query(sqlQuery, parametriQuery, (error, results) => {
+
+        //gestisco il caso in cui la query va in errore
+        if (error){
+            return res.status(500).json({error:"DB error", message: `Errore nel recuperare il post con id ${id} dal db`})
+        }
+        
+        //gestisco il caso in cui non ottengo risultati
+        if (results.length === 0) {
+            return res.status(404).json({error:"Not Found", message: `Post con id ${id} non trovato`})
+        }
+
+        //se tutto va bene, restituisco il post trovato
+        res.json(results[0]);
+
+    })
+
 }
 
 //creo una funzione di destroy che elimina un post in base all'id
